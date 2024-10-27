be3a8c3c023875670986c7cb669fbc98f35e40ec
andosty <35182275+andosty@users.noreply.github.com>
2024-10-27 15:40:48 +0000

    Update 01_A_download_merge.R

--- server/hqdata/01_A_download_merge.R
+++ server/hqdata/01_A_download_merge.R
@@ -204,7 +204,11 @@ for(i in 1:nrow(user_assigned_data)) {
   filter(
     parse_number(team_number)>=row$startTeamNumber &   parse_number(team_number)<=row$endTeamNumber
   ) %>%
-    arrange(RegionName, districtName, townCity, borderPostName, team_number, enumerator_name )
+    arrange(RegionName, districtName, townCity, borderPostName, team_number, enumerator_name ) %>%
+   %>%
+  mutate( #fix dataset issues
+    productObserved=  gsub("colanut", "cola nut", productObserved, ignore.case = TRUE) ,
+  )
     
 
   icbt_data <- dplyr::bind_rows(icbt_data, filteredDataset)
