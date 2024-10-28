cadd6d52f3bad54a0194d718258146e305cfce1e
andosty <35182275+andosty@users.noreply.github.com>
2024-10-28 09:44:38 +0000

    additional error check on trade direction

--- server/hqdata/03_errorchecks.R
+++ server/hqdata/03_errorchecks.R
@@ -59,6 +59,50 @@ blankCommdtyDesctip <- downloaded_icbt_data %>%
 errorChecks <- dplyr::bind_rows(errorChecks, blankCommdtyDesctip) #add to the errorData frame file
 rm(blankCommdtyDesctip)
 
+#Direction of Trade error
+directionErrror <- downloaded_icbt_data %>% 
+  filter( 
+    str_detect(observedRespondentDescription,'coming in|going out')
+    # str_detect(observedRespondentDescription,'coming in') | str_detect(observedRespondentDescription,'going out') 
+    # grepl('coming in|going out', observedRespondentDescription, ignore.case=TRUE) 
+  ) %>%
+  distinct(interview_key, interview_id, transpondent_id, .keep_all = T) %>%
+  mutate(
+    direction = as.character(paste(tradeDirection)),
+    # tradeDirection<- gsub('[[:punct:] ]+',' ',tradeDirection),
+    direction = str_remove_all(direction,"(Export)"),
+    direction = str_remove_all(direction,"(Import)"),
+    direction= gsub('[[:punct:] ]+',' ',direction),
+    direction = str_squish(trim(trimws(str_to_lower(direction)))),
+    observedRespondentDescription= str_squish(trim(trimws(str_to_lower(observedRespondentDescription))))
+  ) %>%
+  mutate(
+    badDir = case_when(
+      # str_detect(string, pattern, negate = FALSE)
+      str_detect(observedRespondentDescription,direction)  ~ 0,
+      # stri_detect_fixed(observedRespondentDescription ,direction) 
+      TRUE ~ 1
+    )
+  ) %>%
+  filter(badDir==1) %>%
+  mutate(
+    errorCheck = 'Trade Direction Error',
+    errorMessage = paste("trade direction selected ='",tradeDirection, "', but description of transpondent ='",observedRespondentDescription,"'", sep = '')
+  ) %>% select(.,interview_key,interview_id,observedRespondentDescription, transpondent_id,commodityObervedDescription,Commodity_id, errorCheck,errorMessage)
+errorChecks <- dplyr::bind_rows(errorChecks, directionErrror) #add to the errorData frame file
+rm(directionErrror)
+
+tradeDirectionNotSpecified <- downloaded_icbt_data %>% 
+  filter( 
+    !str_detect(observedRespondentDescription,'coming in|going out')
+  ) %>%
+  distinct(interview_key, interview_id, transpondent_id, .keep_all = T) %>%
+  mutate(
+    errorCheck = 'Trade Direction Missing',
+    errorMessage = paste("Direction of respondent not specified in description of transpondent ='",observedRespondentDescription,"'", sep = '')
+  ) %>% select(.,interview_key,interview_id,observedRespondentDescription, transpondent_id,commodityObervedDescription,Commodity_id, errorCheck,errorMessage)
+errorChecks <- dplyr::bind_rows(errorChecks, tradeDirectionNotSpecified) #add to the errorData frame file
+rm(tradeDirectionNotSpecified)
 
 UoM_Check <- downloaded_icbt_data %>% 
   filter(!is.na(commodityObervedDescription)) %>%
